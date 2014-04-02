2ad9d8f09afde5da79f4d258059cf3bef528b675
Keith Michael Gregg <keithmichaelgregg@Keiths-MacBook-Pro.local>
2014-04-02 20:34:40 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ariaquen::Application.config.secret_token = 'd20b19e4435c6f60e1533da1d1ed9320b3c931c4ca92ef7fecb0dbb6310b42d6c2596311a1daa85806145e8a26a2a6f36bba44a8400a6a7fbdd7a500a5fa23c8'
