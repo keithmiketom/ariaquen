fb8575e3bb4ce9f7de7849a7fa7740ac18b0f306
Sandra McCormack <sandramccormack@FufuMac.local>
2014-04-30 16:35:26 +0100

    seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140410003527) do
+ActiveRecord::Schema.define(:version => 20140423174207) do
+
+  create_table "designs", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "installs", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create ( :product_name => 'V-Neck Tshirt', :product_detail => '100% cotton v-neck tee by American Apparel', :price => '18.95', :product_category => 'Tops', :in_stock => 'True', :gender => 'Womens', :image_url1 => 'aa_white_vneck_tee1.jpeg', :image_url2 => 'aa_white_vneck_tee2.jpeg', :image_url3 => 'aa_white_vneck_tee3.jpeg')
+
+Product.create ( :product_name => 'Short Sleeve Tshirt', :product_detail => 'poly-cotton short sleeve tshirt by American Apparel', :price => '17.95', :product_category => 'Tops', :in_stock => 'True', :gender => 'Womens', :image_url1 => 'aa_poly_cotton_short_sleeve1.jpeg', :image_url2 => 'aa_poly_cotton_short_sleeve2.jpeg', :image_url3 => 'aa_poly_cotton_short_sleeve3.jpeg')
+
+Product.create ( :product_name => 'Reversible Sweater', :product_detail => '100% cotton reversible sweater by American Apparel', :price => '22.95', :product_category => 'Sweaters', :in_stock => 'True', :gender => 'Womens', :image_url1 => 'aa_reversible_sweater1.jpeg', :image_url2 => 'aa_reversible_sweater2.jpeg', :image_url3 => 'aa_reversible_sweater3.jpeg')
+
+Product.create ( :product_name => 'Ottoman Sweater', :product_detail => 'Ottoman Sweater by American Apparel', :price => '21.95', :product_category => 'Sweaters', :in_stock => 'True', :gender => 'Womens', :image_url1 => 'aa_ottoman_sweater1.jpeg', :image_url2 => 'aa_ottoman_sweater2.jpeg', :image_url3 => 'aa_ottoman_sweater3.jpeg')
+
+Product.create ( :product_name => 'Vest Dress', :product_detail => '100% cotton vest-style dress by American Apparel', :price => '19.99', :product_category => 'Dresses', :in_stock => 'True', :gender => 'Womens', :image_url1 => 'aa_white_vest_dress1.jpeg', :image_url2 => 'aa_white_vest_dress1.jpeg', :image_url3 => 'aa_white_vest_dress3.jpeg')
+
+Product.create ( :product_name => 'Tshirt Dress', :product_detail => '100% cotton tshirt dress by American Apparel', :price => '21.95', :product_category => 'Dresses', :in_stock => 'True', :gender => 'Womens', :image_url1 => 'aa_cotton_tshirt_dress1.jpeg', :image_url2 => 'aa_cotton_tshirt_dress2.jpeg', :image_url3 => 'aa_cotton_tshirt_dress3.jpeg')
+
+Product.create ( :product_name => 'Short Sleeve Tshirt', :product_detail => '100% cotton tee by American Apparel', :price => '18.95', :product_category => 'Tops', :in_stock => 'True', :gender => 'Mens', :image_url1 => 'aa_mens_white_tee1.jpeg', :image_url2 => 'aa_mens_white_tee2.jpeg', :image_url3 => 'aa_mens_white_tee3.jpeg')
+
+Product.create ( :product_name => 'V-Neck Tshirt', :product_detail => '100% cotton v-neck tee by American Apparel', :price => '16.99', :product_category => 'Tops', :in_stock => 'True', :gender => 'Mens', :image_url1 => 'aa_mens_vneck_tee1.jpeg', :image_url2 => 'aa_mens_vneck_tee2.jpeg', :image_url3 => 'aa_mens_vneck_tee3.jpeg')
+
+Product.create ( :product_name => 'Ottoman Sweater', :product_detail => 'Ottoman sweater by American Apparel', :price => '21.95', :product_category => 'Sweaters', :in_stock => 'True', :gender => 'Mens', :image_url1 => 'aa_mens_ottoman_sweater1.jpeg', :image_url2 => 'aa_mens_ottoman_sweater2.jpeg', :image_url3 => 'aa_mens_ottoman_sweater3.jpeg')
+
+Product.create ( :product_name => 'Crew-Neck Sweater', :product_detail => '100% cotton crew-neck sweater by American Apparel', :price => '20.95', :product_category => 'Sweaters', :in_stock => 'True', :gender => 'Mens', :image_url1 => 'aa_mens_basic_crew_neck_sweater1.jpeg', :image_url2 => 'aa_mens_basic_crew_neck_sweater2.jpeg', :image_url3 => 'aa_mens_basic_crew_neck_sweater3.jpeg')
+
+Product.create ( :product_name => 'Cap Sleeve Tshirt', :product_detail => 'Kids baby rib cap-sleeve tshirt by American Apparel', :price => '8.95', :product_category => 'Tops', :in_stock => 'True', :gender => 'Kids', :image_url1 => 'kids_baby_rib_cap_sleeve_tee1.jpeg', :image_url2 => 'kids_baby_rib_cap_sleeve_tee2.jpeg', :image_url3 => 'kids_baby_rib_cap_sleeve_tee3.jpeg')
+
+Product.create ( :product_name => 'Neon Tshirt', :product_detail => 'Kids neon poly-cotton tshirt by American Apparel', :price => '8.99', :product_category => 'Tops', :in_stock => 'True', :gender => 'Kids', :image_url1 => 'kids_neon_poly_cotton_tee1.jpeg', :image_url2 => 'kids_neon_poly_cotton_tee2.jpeg', :image_url3 => 'kids_neon_poly_cotton_tee3.jpeg')
+
+Product.create ( :product_name => 'Babygro', :product_detail => '100% cotton babygro by American Apparel', :price => '7.49', :product_category => 'Tops', :in_stock => 'True', :gender => 'Kids', :image_url1 => 'aa_babygro_1.jpeg', :image_url2 => 'aa_babygro_2.jpeg', :image_url3 => 'aa_babygro_3.jpeg')
+
+Product.create ( :product_name => 'Kids Racerback Dress', :product_detail => 'Kids ribbed racerback dress by American Apparel', :price => '10.95', :product_category => 'Dresses', :in_stock => 'True', :gender => 'Kids', :image_url1 => 'kids_dress1.jpeg', :image_url2 => 'kids_dress2.jpeg', :image_url3 => 'kids_dress3.jpeg')
+
+Product.create ( :product_name => 'Kids Highlighter Dress', :product_detail => 'Kids baby rib highlighter dress by American Apparel', :price => '10.95', :product_category => 'Dresses', :in_stock => 'True', :gender => 'Kids', :image_url1 => 'kids_baby_rib_highlighter_dress1.jpeg', :image_url2 => 'kids_baby_rib_highlighter_dress2.jpeg', :image_url3 => 'kids_baby_rib_highlighter_dress3.jpeg')
+
+
