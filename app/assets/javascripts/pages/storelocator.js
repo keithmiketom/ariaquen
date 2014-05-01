9c37b6851fdabe143ecb5d81a7b1eeef9433d1d6
miguel-castillo <miguel_castillo@me.com>
2014-05-01 21:24:38 +0100

    changes latina

--- app/assets/javascripts/pages/storelocator.js
+++ app/assets/javascripts/pages/storelocator.js
@@ -1,18 +1,3 @@
 
 
-	$(function() {
-  $('#map_canvas').gmap().bind('init', function() { 
-  	// This URL won't work on your localhost, so you need to change it
-	// see http://en.wikipedia.org/wiki/Same_origin_policy
-	$.getJSON( 'stores.json', function(data) { 
-		$.each( data.markers, function(i, marker) {
-			$('#map_canvas').gmap('addMarker', { 
-				'position': new google.maps.LatLng(marker.latitude, marker.longitude), 
-				'bounds': true 
-			}).click(function() {
-				$('#map_canvas').gmap('openInfoWindow', { 'content': marker.content }, this);
-			});
-		});
-	});
-});
-});
+	
